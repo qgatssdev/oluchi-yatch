482d4dd5b27e426791df18c6e3278b703ea62a20
Uche Omodu <chivoomodu@gmail.com>
2022-11-13 15:57:49 +0100

    add Loader

---
+++ src/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className='Loader'>
+      <p>RELAXXXXXX. LOADING...</p>
+    </div>
+  );
+};
+
+export default Loader;
